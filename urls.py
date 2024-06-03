bd1dcda2be08bf1146fb42909af83b9665d62e02
Adrian <adriangomis34@gmail.com>
2024-06-03 13:27:44 +0200

    Arreglando problemas de código

--- urls.py
+++ urls.py
@@ -1,8 +1,17 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .import views
 
-url_patterns = [
-    
-    path("admin/", admin.site.urls),
-    path("", include("Home.urls")),
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('signup', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
+    path("logout/", views.logout, name="logout"),
+    path("dashboard/", views.dashboard, name="dashboard"),
+    path("communityposts/", views.communityposts, name="communityposts"),
+    path("update_profile/", views.updateprofile, name="updateprofile"),
+    path("addlesson/", views.addlesson, name= "addlesson"),
+    path("favorite_language/", views.favorite_language, name="select_favorite_language"),
+    path("language/<int:language_id>/", views.language, name="language"),
+    path("lessons_detail/<int:lesson_id>/", views.lesson_detail, name="lesson_detail"),
+    path("create_community_post/", views.create_community_post, name="create_community_post"),
 ]
