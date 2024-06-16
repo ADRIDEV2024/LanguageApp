0531face30b0167887fae7bdeb7c4c6e7f820f82
Adrian <adriangomis34@gmail.com>
2024-06-16 11:51:14 +0200

    Update views.py

--- views.py
+++ views.py
@@ -10,7 +10,6 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.urls import reverse_lazy
 
 def index(request):
-
     return render(request, 'index.html')
 
 def dashboard(request):
