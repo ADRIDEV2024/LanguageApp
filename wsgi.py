10189d21981db9f8a1ce5c26d865bc71f8583eba
Adrian <adriangomis34@gmail.com>
2024-07-07 10:33:37 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -5,6 +5,10 @@ from django.core.wsgi import get_wsgi_application
 django_wsgi_app = get_wsgi_application()
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LanguageApp.settings")
+
+
+
+
 application = ProtocolTypeRouter({ "http": django_wsgi_app,})
 
 WSGI_APPLICATION = 'LanguageApp.wsgi.application'
