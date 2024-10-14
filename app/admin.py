214d26dc1de4b7d625a4ae2b139524af4ee0059a
Adrian <adriangomis34@gmail.com>
2024-10-14 12:27:48 +0200

    Update admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Lesson, UserProfile, Language, UserLanguage, 
 LessonTag, CommunityPost
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'language', 'created_at')
+    list_filter = ('language', 'tags')
+    search_fields = ('title', 'description')
+    ordering = ('-created_at',)
  
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(UserProfile, UserProfileAdmin)
