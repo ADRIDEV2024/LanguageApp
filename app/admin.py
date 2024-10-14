bf6aca3d83805d0a838264a73695044c341b088a
Adrian <adriangomis34@gmail.com>
2024-10-14 12:26:25 +0200

    Update admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from .models import Lesson, UserProfile, Language, UserLanguage, LessonTag, CommunityPost
+from .models import Lesson, UserProfile, Language, UserLanguage, 
+LessonTag, CommunityPost
  
-admin.site.register(Lesson)
-admin.site.register(UserProfile)
-admin.site.register(Language)
-admin.site.register(UserLanguage)
-admin.site.register(LessonTag)
-admin.site.register(CommunityPost)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(Language, LanguageAdmin)
+admin.site.register(UserLanguage, UserLanguageAdmin)
+admin.site.register(LessonTag, LessonTagAdmin)
+admin.site.register(CommunityPost, CommunityPostAdmin)
 
 
